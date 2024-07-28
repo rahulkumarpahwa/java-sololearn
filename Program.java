39e231c4e0485cd2a26fe4fd6dbf94084015a707
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-07-28 20:56:06 +0530

    lesson 4

--- Program.java
+++ Program.java
@@ -77,3 +77,27 @@ class Demo {
 // chapter 3 finished
 
 //chapter 4: 
+/*Your First Code Coach 
+
+Let's code for real!
+The given program outputs “Coding is boring”.
+But that’s not true, is it? Let’s change the message.
+
+Task:
+Modify the code to output "Coding is fun" to the screen. */
+
+class Demo{
+    public static void main(String[] args) {
+        System.out.println("Coding is boring");
+    }
+}
+
+//solution :
+
+class Demo{
+    public static void main(String[] args) {
+        System.out.println("Coding is fun");
+    }
+}
+
+// chapter 4 finished.
