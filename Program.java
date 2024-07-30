01853558630ad993435a4574589d3756f09732d1
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-07-30 17:09:55 +0530

    chapter 10

--- Program.java
+++ Program.java
@@ -412,3 +412,38 @@ class Demo {
 
 // Chapter 9 Finished.
 
+//chapter 10:
+/*Chess Tournament Scores
+
+You are making a program for a chess tournament, that needs to calculate the points earned by a player.
+
+A win is worth 1 point, while a tie is worth 0.5 points.
+
+The given program declares two variables: wins and ties with the corresponding values.
+
+Create a program to calculate and output the points earned by the player. */
+//Multiply the ties value by 0.5, to get the points earned for ties.
+
+public class Program {
+    public static void main(String[] args) {
+        int wins = 54;
+        int ties = 31;
+
+        
+    }
+}
+
+// my solution : 
+
+public class Program {
+    public static void main(String[] args) {
+        int wins = 54;
+        int ties = 31;
+
+        double total = wins * 1 + ties * 0.5;
+        System.out.println(total);
+    }
+}
+
+
+// chapter 10 finished.
