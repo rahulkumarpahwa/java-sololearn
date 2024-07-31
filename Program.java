c6b0df87b89b92bd0f3010874a43d6e9d0741e10
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-01 00:08:27 +0530

    chapter 7

--- Program.java
+++ Program.java
@@ -408,3 +408,72 @@ public class Program {
 }
 
 //chapter 6 Finished.
+
+//chapter 7:
+
+// Multiple Conditions
+
+// In some cases we need to combine multiple conditions, for example, let's say we want to check if the <b>age </b>value is greater than 18 and less than 50.
+
+// This can be done using the && operator.
+
+class Demo {
+    public static void main(String[ ] args) {
+        int age = 42;
+        if (age > 18 && age < 50) {
+            System.out.println("Welcome!");
+        }
+    }
+}
+
+//The && operator is also referred to as the logical AND operator.
+
+//The OR Operator
+
+// The OR operator (<b>||</b>) checks if any one of the conditions is true. For example:
+
+class Demo {
+    public static void main(String[ ] args) {
+        int age = 25;
+        int height = 100;
+
+        if (age > 18 || height > 150) {
+            System.out.println("Welcome!");
+        }
+    }
+}
+
+//The code above will print Welcome! if <b>age </b>is greater than 18 or if <b>height </b>is greater than 150.
+
+//NOT
+
+// The NOT (!) logical operator is used to reverse the condition. If a condition is true, the NOT logical operator will make it false, and vice versa. Example:
+
+class Demo {
+    public static void main(String[ ] args) {
+        int age = 25;
+        if(!(age > 18)) {
+            System.out.println("Too Young");
+        } else {
+            System.out.println("Welcome");
+        }
+    }
+}
+
+//!(age > 18) reads as "if age is NOT greater than 18".
+
+//You can chain multiple conditions using parentheses and the logical operators.
+// For example:
+class Demo {
+    public static void main(String[ ] args) {
+        String country = "US";
+        int age = 42;
+
+        if((country == "US" || country == "GB") && (age > 0 && age < 100)) {
+            System.out.println("Allowed");
+        }
+    }
+}
+
+// chapter 7 Finished.
+
