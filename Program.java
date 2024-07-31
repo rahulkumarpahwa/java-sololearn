1123eed99eb9782346ac6eff470f05e5f177be61
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-07-31 13:19:01 +0530

    chapter 4

--- Program.java
+++ Program.java
@@ -201,3 +201,48 @@ class Demo {
 // You can include as many else if statements as you need.
 
 //chapter 3 Finished.
+
+// chapter 4:
+//PRACTICE EXERCISE
+// Boiling Water 
+
+// You are making a program for a water sensor that should check if the water is boiling.
+// Take the integer temperature in Celsius as input and output "Boiling" if the temperature is above or equal to 100.
+// Output "Not boiling" if it's not.
+
+// Sample Input
+// 105
+
+// Sample Output
+// Boiling
+
+//Make sure to output the text exactly as defined in the text.
+
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        //your code goes here
+        
+    }
+}
+
+//my solution:
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double temp = sc.nextDouble();
+        if(temp>=100){
+            System.out.println("Boiling");
+        } else{
+            System.out.println("Not boiling");
+        }
+        
+    }
+}
+
+// chapter 4 Finished.
+
+// chapter 5:
