30e239e2a27f5e70b7b27a23e744be2aa6864fa4
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-07-31 13:02:37 +0530

    chapter 2

--- Program.java
+++ Program.java
@@ -55,4 +55,37 @@ public class Program {
 
 // chapter 1 Finished.
 
+//chapter 2:
+// PRACTICE EXERCISE
+// Tip Calculator 
+
+// You always tip 15% of the bill amount. 
+// To make the calculation easier, you decide to write a program that will do that for you.
+// Task: Take the bill amount as input and output the corresponding tip amount, which should be 15% of the amount.
+// To calculate 15% of a number, multiply it by 15, then divide by 100.
+// The input amount can be a decimal, so take a double from the input.
+
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        //your code goes here
+        
+    }
+}
+
+//my solution :
+
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double amount = sc.nextDouble(); 
+        System.out.println(15 * amount / 100);
+        
+    }
+}
+
+//chapter 2 Finished.
 
