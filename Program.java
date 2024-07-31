9c66d09bde409cee5d81397acf301f030d4a78bf
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-01 00:00:02 +0530

    chapter 6

--- Program.java
+++ Program.java
@@ -354,4 +354,57 @@ class Demo {
 //chapter 5 Finished.
 
 //chapter 6:
+// PRACTICE EXERCISE
+// Boxes
 
+// You are making a robot that should categorize items by their color.
+// Each color corresponds to a box with a specific number.
+// For simplicity, our program will handle 3 colors:
+
+// red goes to box #1
+// green goes to box #2
+// black goes to box #3
+
+// Your program needs to take a color as input and output the corresponding box number.
+
+// Sample Input
+// green
+// Sample Output
+// 2
+
+// Remember, you can use the switch statement to check for different conditions.
+
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        //your code goes here
+        
+    }
+}
+
+//my solution:
+
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        switch(input){
+            case "red":
+            System.out.println(1);
+               break;
+            case "green":
+            System.out.println(2);
+               break;
+            case "black":
+            System.out.println(3);
+               break;
+        }
+        
+    }
+}
+
+//chapter 6 Finished.
