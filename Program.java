7d5d9d8bbbe90d07c8804a519fed90847dcf0fdf
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-07-31 23:52:34 +0530

    chapter 5

--- Program.java
+++ Program.java
@@ -246,3 +246,112 @@ public class Program {
 // chapter 4 Finished.
 
 // chapter 5:
+//Conditionals
+// Consider a program that takes a day number as input and outputs the corresponding weekday:
+
+class Demo {
+    public static void main(String[ ] args) {
+        int day = 2;
+        if(day == 1) {
+          System.out.println("Monday");
+        } else if(day == 2) {
+          System.out.println("Tuesday");
+        } else if(day == 3) {
+          System.out.println("Wednesday");
+        }
+    }
+}
+
+//For a shorter code in our demo, we have checked only for the first 3 values.You can continue the code and check for all 7 day numbers.
+
+// switch
+// Instead of many if else statements, which become hard to read, we can use a switch statement.
+// A switch statement tests a variable for equality against a list of values.
+// Here is the previous example using a switch statement:
+
+class Demo {
+    public static void main(String[ ] args) {
+        int day = 2;
+
+        switch(day) {
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        }
+    }
+}
+
+//Let's look at the code again:
+class Demo {
+    public static void main(String[ ] args) {
+        int day = 2;
+
+        switch(day) {
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        }
+    }
+}
+
+//When the variable being switched on is equal to a case, the statements following that case will execute until a break statement is reached.
+//You can have any number of case statements within a switch. Each case is followed by the comparison value and a colon.
+
+//It is important to have a <b>break </b>statement for each case.
+// If no <b>break </b>appears, the program will continue to execute the next <b>case </b>in the switch, even if the value does not match the variable that is switched on.
+// Run this example to see what happens when there is no break in the case:
+
+class Demo {
+    public static void main(String[ ] args) {
+        int day = 2;
+
+        switch(day) {
+        case 1:
+            System.out.println("Monday");
+        case 2:
+            System.out.println("Tuesday");
+        case 3:
+            System.out.println("Wednesday");
+        }
+    }
+}
+
+//The default Case
+// A switch statement can have an optional <b>default </b>case.
+// The <b>default </b>case can be used for performing a task when none of the cases is matched.
+// For example:
+
+class Demo {
+    public static void main(String[ ] args) {
+        int day = 5;
+
+        switch(day) {
+        case 1:
+            System.out.println("Monday");
+        case 2:
+            System.out.println("Tuesday");
+        case 3:
+            System.out.println("Wednesday");
+        default: 
+            System.out.println("Another day");
+        }
+    }
+}
+//No <b>break </b>is needed in the default case, as it is always the last statement in the switch.
+
+//chapter 5 Finished.
+
+//chapter 6:
+
