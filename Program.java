2c3d3bef9ec33c8caeea8704f40fb172714e5c01
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-01 00:15:11 +0530

    chapter 8

--- Program.java
+++ Program.java
@@ -477,3 +477,53 @@ class Demo {
 
 // chapter 7 Finished.
 
+//chapter 8:
+//PRACTICE EXERCISE
+// Age Group
+
+// Given the age of a person as an input, output their age group. Here are the age groups you need to handle:
+
+// Child: 0 to 11
+// Teen: 12 to 17
+// Adult: 18 to 64
+
+// Sample Input
+// 42
+// Sample Output
+// Adult
+
+// Remember, you can use the AND operator to combine conditions, like x>0 && x<20.
+
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        //your code goes here
+        
+    }
+}
+
+//my solution:
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        
+        if(age>=0 && age<=11){
+            System.out.println("Child");
+        } else if(age>=12 && age<=17){
+            System.out.println("Teen");
+        } else if(age>=18 && age<=64){
+            System.out.println("Adult");
+        } 
+        
+    }
+}
+
+//chapter 8 Finished.
+
+
