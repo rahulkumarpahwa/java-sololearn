445417e08012069311af348bf51dea59a388cd16
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-01 10:36:57 +0530

    chapter 11

--- Program.java
+++ Program.java
@@ -692,3 +692,101 @@ public class Program {
 
 // chapter 10 Finished.
 
+//chapter 11:
+//for Loops
+
+// Another kind of loop is the <b>for </b>loop. It looks like this:
+
+class Demo {
+    public static void main(String[ ] args) {
+        for(int x=1; x<5; x++) {
+            System.out.println(x);
+        }
+    }
+}
+
+//This will output the numbers 1 through 4.
+//The for loop has 3 components:
+
+class Demo {
+    public static void main(String[ ] args) {
+        for(int x=1; x<5; x++) {
+            System.out.println(x);
+        }
+    }
+}
+
+//The first part runs once when we enter the loop and initializes the variable. The second part is the condition of the loop.The third part runs every time the loop runs.
+//Notice the semicolons (;) after the parts of the loop.
+
+//You can have any type of condition and any type of increment statements in the <b>for </b>loop.The example below prints only the even values between 0 and 10:
+
+class Demo {
+    public static void main(String[ ] args) {
+        for(int x=0; x<=10; x+=2) {
+            System.out.println(x);
+        }
+    }
+}
+
+// The <b>for </b>loop is best when we know the number of times we need to run the loop.
+
+//Loop Control
+
+// Remember the <b>break </b>statement from the <b>switch</b>? It can also be used to terminate loops. Example:
+
+class Demo {
+    public static void main(String[ ] args) {
+        int x = 1;
+
+        while(x < 10) {
+            System.out.println(x);
+            if(x == 4) {
+                break;
+            }
+            x++;
+        }
+    }
+}
+
+//This will end the loop when x reaches the value 4.
+//break
+// It also works in the for loop:
+
+class Demo {
+    public static void main(String[ ] args) {
+        for(int x=1; x<10; x++) {
+            System.out.println(x);
+            if(x == 4) {
+                break;
+            }
+        }
+    }
+}
+
+//Here is one example use case of break:
+
+// For example, you are making a calculator and need to take numbers from the user to add together and stop when the user enters "stop". In this case, the break statement can be used to end the loop when the user input equals "stop".
+
+// continue
+
+// Another control statement is <b>continue</b>.It makes the loop skip the current iteration and go to the next one.
+//Example:
+
+class Demo {
+    public static void main(String[ ] args) {
+        for(int x=10; x<=40; x+=10) {
+            if(x == 30) {
+                continue;
+            }
+            System.out.println(x);
+        }
+    }
+}
+
+//The above code skips the value of 30, as directed by the continue statement. 
+
+// An example use case of <b>continue</b>:
+// An airline ticketing system needs to calculate the total cost for all tickets purchased. Tickets for children under the age of 3 are free. We can use a while loop to iterate through the list of passengers and calculate the total cost of their tickets. Here, the continue statement can be used to skip the children.
+
+// chapter 11 Finished.
