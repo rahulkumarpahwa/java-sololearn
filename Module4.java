d856a67cd3efdb3d5f03cad859e8cc560009628f
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-03 00:55:45 +0530

    chapter 6

--- Module4.java
+++ Module4.java
@@ -298,5 +298,51 @@ class Demo {
 
 // chapter 5 Finished.
 
+// chapter 6: 
 
+// PRACTICE EXERCISE
+// Celsius to Fahrenheit
 
+// You are making a Celsius to Fahrenheit converter. 
+// Write a method to take the Celsius value as an argument and return the corresponding Fahrenheit value.
+
+// Sample Input
+// 36
+
+// Sample Output
+// 96.8
+
+// The given code takes the celsius value as input and passes it to a <b>fahr()</b> method, which you need to create.
+// The following equation is used to calculate the Fahrenheit value: 1.8 * celsius + 32.
+
+import java.util.Scanner;
+
+public class Program {
+    //your code goes here
+    
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+        double c = sc.nextDouble(); 
+
+        System.out.println(fahr(c));
+    }
+}
+
+// my solution:
+import java.util.Scanner;
+
+public class Program {
+    static double fahr(double c){
+        double res = 1.8 * c + 32;
+        return res;
+    }
+    
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+        double c = sc.nextDouble(); 
+
+        System.out.println(fahr(c));
+    }
+}
+
+// chapter 6 Finished.
