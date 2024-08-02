a358d73c494541c2746b92703a65746d209f11e6
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-02 23:53:45 +0530

    chapter 2

--- Module4.java
+++ Module4.java
@@ -65,3 +65,54 @@ class Demo {
 }
 
 // chapter 1 Finished.
+
+// chapter 2:
+// PRACTICE EXERCISE
+// Bot
+// You are making an automated response program for a store.
+// The bot should take a number from the user as input and reply with an automated message.
+// There are currently 3 responses, that you need to a handle:
+// User message: "1", Reply: "Order confirmed"
+// User message: "2", Reply: "info@sololearn.com"
+// For any other number, the reply should be: "Try again".
+// The given code calls a method called bot(). Define the method, which should take an integer input from the user, and handle the above mentioned cases, by outputting the corresponding reply.
+// Do not change the method call in <b>main</b>().
+
+import java.util.Scanner;
+
+public class Program {
+    //your code goes here
+    /*
+    User message: "1", Reply: "Order confirmed"
+    User message: "2", Reply: "info@sololearn.com"
+    For any other number, the reply should be: "Try again". 
+    */
+    public static void main(String[] args) {
+        bot();
+    }
+}
+
+// my solution:
+import java.util.Scanner;
+
+public class Program { 
+
+    static void bot(){
+    Scanner sc = new Scanner(System.in);
+    int input = sc.nextInt(); 
+
+    if(input == 1){
+        System.out.println("Order confirmed");
+    } else if (input == 2){
+        System.out.println("info@sololearn.com");
+    } else {
+        System.out.println("Try again");
+    }
+
+    }
+    public static void main(String[] args) {
+        bot();
+    }
+}
+
+// chapter 2 Finished.
