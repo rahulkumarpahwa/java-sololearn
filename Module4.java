66a512c45d146920f133346dc94669caabeeee00
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-08-03 00:37:27 +0530

    chapter 4

--- Module4.java
+++ Module4.java
@@ -181,3 +181,45 @@ class Demo {
 }
 
 // chapter 3 Finished.
+
+//chapter 4:
+// PRACTICE EXERCISE
+// Feet to Inches Converter
+// You need to make a method that converts a foot value to inches.
+// 1 foot has 12 inches. 
+// Define a <b>convert()</b> method, that takes the foot value as its argument and outputs the inches value.
+// The result must be a double.
+
+//The given code takes the foot value as input and passes it to the convert method. Define the convert method, so that the given code works.
+
+import java.util.Scanner;
+
+public class Program {
+    //your code goes here
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num = sc.nextDouble(); 
+
+        convert(num);
+    }
+}
+
+//my solution: 
+
+import java.util.Scanner;
+
+public class Program {
+     static void convert(double foot){
+         double res = foot * 12;
+            System.out.println(res);
+        }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num = sc.nextDouble(); 
+        convert(num);
+    }
+}
+
+// chapter 4 Finished.
